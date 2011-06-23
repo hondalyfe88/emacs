e795464408c450d46f3fdd4b6727947519361125
Matthew McCullough <matthewm@ambientideas.com>
2011-06-23 00:17:08 -0600

    Added XML formatting

--- init.el
+++ init.el
@@ -11,6 +11,24 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; customizations
 
+;; XML Formatter
+;; from http://sinewalker.wordpress.com/2008/06/26/pretty-printing-xml-with-emacs-nxml-mode/
+(defun nxml-pretty-print-region (begin end)
+  "Pretty format XML markup in region. You need to have nxml-mode
+http://www.emacswiki.org/cgi-bin/wiki/NxmlMode installed to do
+this.  The function inserts linebreaks to separate tags that have
+nothing but whitespace between them.  It then indents the markup
+by using nxml's indentation rules."
+  (interactive "r")
+  (save-excursion
+      (nxml-mode)
+      (goto-char begin)
+      (while (search-forward-regexp "\>[ \\t]*\<" nil t)
+        (backward-char) (insert "\n"))
+      (indent-region begin end))
+    (message "Ah, much better!"))
+
+
 ;;;;;;;;;;;;;;;;
 ;; color theme
 (add-to-list 'load-path "~/.emacs.d/color-theme")

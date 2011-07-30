fd985424df5682009cb2ae230ad6c4def93b11cb
Matthew McCullough OSCON <matthewm@ambientideas.com>
2011-07-30 14:28:51 -0700

    Added ELPA package manager

--- init.el
+++ init.el
@@ -251,3 +251,14 @@ by using nxml's indentation rules."
 
 ;(global-set-key [kbd "M-s"] 'save-buffer)
 ;(global-set-key [kbd "M-z"] 'undo)
+
+
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))

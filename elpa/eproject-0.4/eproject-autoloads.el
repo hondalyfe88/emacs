cb5a711b0766cba239fdafd7a96a7aeb0832c802
Matthew McCullough OSCON <matthewm@ambientideas.com>
2011-08-02 16:01:14 -0700

    Added many ELPA packages

---
+++ elpa/eproject-0.4/eproject-autoloads.el
@@ -0,0 +1,24 @@
+;;; eproject-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "eproject" "eproject.el" (20020 30713))
+;;; Generated autoloads from eproject.el
+(require 'eproject)
+
+;;;***
+
+;;;### (autoloads nil nil ("eproject-config.el" "eproject-pkg.el")
+;;;;;;  (20020 30713 593096))
+
+;;;***
+
+(provide 'eproject-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; eproject-autoloads.el ends here
